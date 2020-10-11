4e0073526646b090344941183a1942e8e32b9fea
progressivePRV <94014.prabhav@gmail.com>
2020-10-10 23:38:26 -0400

    added post nounce to server

--- shoppingCartApp/app/src/main/java/com/example/shoppingcartapp/PaymentActivity.java
+++ shoppingCartApp/app/src/main/java/com/example/shoppingcartapp/PaymentActivity.java
@@ -19,11 +19,18 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class PaymentActivity extends AppCompatActivity {
+    private static final String TAG = "okay";
     private float totalPrice = 10;
 
     SharedPreferences preferences;
@@ -90,6 +97,7 @@ public class PaymentActivity extends AppCompatActivity {
     public void onBraintreeSubmit(String clientToken) {
         DropInRequest dropInRequest = new DropInRequest()
                 .clientToken(clientToken);
+        dropInRequest.collectDeviceData(true);
         startActivityForResult(dropInRequest.getIntent(this), 150);
     }
 
@@ -100,12 +108,18 @@ public class PaymentActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 DropInResult result = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
                 // use the result to update your UI and send the payment method nonce to your server
-                Log.d("demo", "It is coming inside the result OK.. Yay!!!");
-//                Log.d("deviceData", result.getDeviceData());
+                Log.d(TAG, "It is coming inside the result OK.. Yay!!!");
+                Log.d(TAG, result.getDeviceData());
 //                Log.d("describeCOntents", String.valueOf(result.describeContents()));
-                Log.d("Nounce", result.getPaymentMethodNonce().getNonce());
+                Log.d(TAG, "Nounce"+result.getPaymentMethodNonce().getNonce());
+                /////////sending data to server
+                String nounce = result.getPaymentMethodNonce().getNonce();
+                String deviceData = result.getDeviceData();
+                sendNounceToSever(deviceData,nounce,"10");
+                /////////
+
             } else if (resultCode == RESULT_CANCELED) {
-                Log.d("demo", "It is coming inside the cancel");
+                Log.d(TAG, "It is coming inside the cancel");
                 // the user canceled
             } else {
                 // handle errors here, an exception may be available in
@@ -113,5 +127,45 @@ public class PaymentActivity extends AppCompatActivity {
                 Log.d("demo", "It is coming inside the error");
             }
         }
+    }
+
+    void sendNounceToSever(String deviceData,String nounce,String amount){
+        final OkHttpClient client = new OkHttpClient();
+
+        RequestBody formBody = new FormBody.Builder()
+                .add("deviceData", deviceData)
+                .add("nounce", nounce)
+                .add("amount", amount)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(getResources().getString(R.string.endPointUrl)+"api/v1/shop/checkout")
+                .header("Authorization", "Bearer "+ preferences.getString("TOKEN_KEY", null))
+                .post(formBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    if (!response.isSuccessful())
+                        throw new IOException("Unexpected code " + response);
+
+                    Headers responseHeaders = response.headers();
+                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                    }
+                    System.out.println(responseBody.string());
+                    Toast.makeText(PaymentActivity.this, "sent the nounce data sucessful", Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    Log.d(TAG, "onResponse: error occured while sending the nounce=>"+e.getMessage());
+                }
+            }
+        });
     }
 }
